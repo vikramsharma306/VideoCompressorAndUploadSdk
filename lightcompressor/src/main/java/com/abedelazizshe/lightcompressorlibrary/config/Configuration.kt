da8331a8f2591a7b53168773cf7680465e405e4f
vikram.sharma <vikram.sharma@multitvsolution.com>
2021-11-26 16:18:39 +0530

    Initial commit 04:20 PM 26 Nov 2021

---
+++ lightcompressor/src/main/java/com/abedelazizshe/lightcompressorlibrary/config/Configuration.kt
@@ -0,0 +1,11 @@
+package com.abedelazizshe.lightcompressorlibrary.config
+
+import com.abedelazizshe.lightcompressorlibrary.VideoQuality
+
+data class Configuration(
+    var quality: VideoQuality = VideoQuality.MEDIUM,
+    var frameRate: Int? = null,
+    var isMinBitrateCheckEnabled: Boolean = false,
+    var videoBitrate: Int? = null,
+    var disableAudio: Boolean = false
+)
